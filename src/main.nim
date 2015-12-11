ae06724170ee28c2fe24685846e2f4c049e178dc
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2015-12-11 16:49:54 +0600

    Added nim installation on linux

--- src/main.nim
+++ src/main.nim
@@ -2,7 +2,7 @@ import nimshell, os, logging
 
 when defined windows:
   import private/far_manager
-  proc appRoot(): string = "c:\\apps"
+  proc appRoot: string = "c:\\apps"
 
   proc installOnWindow() =
     setupFarManager tmpDir, appRoot() / "Far"
@@ -17,13 +17,13 @@ elif defined linux:
       fatal "Non debian based systems is not supported yet"
       quit "Non debian based systems is not supported yet"
 
-  proc installGit() =
+  proc installGit =
     if not ?"git".which:
       info "Install git..."
       pkgInstall "git"
     info "Git installed"
 
-  proc installGcc() =
+  proc installGcc =
     if not ?"gcc".which:
       info "Install gcc..."
       pkgInstall "gcc"
@@ -33,17 +33,30 @@ elif defined linux:
       pkgInstall "g++"
     info "G++ installed"
 
+  proc installNim =
+    let nimRoot = appRoot() / "Nim"
+    if nimRoot.dirExists:
+      info "Directory ", nimRoot, " already exists!"
+    else:
+      createDir nimRoot
+      setCurrentDir nimRoot
+      >>! cmd"git clone https://github.com/vegansk/Nim ."
+      >>! cmd"git checkout my"
+      >>! cmd"./bootstrap.sh"
+      
+
   proc installOnLinux() =
     installGit()
     installGcc()
+    installNim()
     
 when isMainModule:
-  logging.addHandler(newConsoleLogger())
+  logging.addHandler newConsoleLogger()
 
   let tmpDir = mktemp()
-  defer: removeDir(tmpDir)
+  defer: removeDir tmpDir
 
-  debug "Temp dir is " & tmpDir
+  debug "Temp dir is ", tmpDir
 
   when defined windows:
     installOnWindows()
