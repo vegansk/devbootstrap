e8ef59daa5e76390fae67f0482eaa79a6f5aa9c2
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2015-12-11 16:28:55 +0600

    Added pkgInstall primitive

--- src/main.nim
+++ src/main.nim
@@ -9,21 +9,29 @@ when defined windows:
   
 elif defined linux:
   proc appRoot(): string = joinPath(getHomeDir(), "apps")
-  proc pkgInstall(pkg: string) =
-    >>! cmd"sudo apt-get install -y ${pkg}" 
+  proc pkgInstall(pkgs: varargs[string]) =
+    if ?"apt-get".which:
+      for pkg in pkgs:
+        >>! cmd"sudo apt-get install -y ${pkg}" 
+    else:
+      fatal "Non debian based systems is not supported yet"
+      quit "Non debian based systems is not supported yet"
 
   proc installGit() =
     if not ?"git".which:
       info "Install git..."
       pkgInstall "git"
+    info "Git installed"
 
   proc installGcc() =
     if not ?"gcc".which:
       info "Install gcc..."
       pkgInstall "gcc"
+    info "Gcc installed"
     if not ?"g++".which:
       info "Install g++..."
       pkgInstall "g++"
+    info "G++ installed"
 
   proc installOnLinux() =
     installGit()
@@ -35,7 +43,7 @@ when isMainModule:
   let tmpDir = mktemp()
   defer: removeDir(tmpDir)
 
-  info "Temp dir is " & tmpDir
+  debug "Temp dir is " & tmpDir
 
   when defined windows:
     installOnWindows()
